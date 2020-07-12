5077072c97f9067ff1e79f39b178a9042a42078c
joydeeprony89 <joydeeprony89@gmail.com>
2020-07-12 21:05:05 +0200

    Add project files.

---
+++ DI String Match/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace DI_String_Match
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            diStringMatch("IDID");
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int[] diStringMatch(string S)
+        {
+            int len = S.Length;
+            int[] arr = new int[len + 1];
+            int I = 0;
+            int D = len;
+            int k = 0;
+
+            for(int i = 0; i< len; i++)
+            {
+                arr[k++] = S[i] == 'D' ? D-- : I++;
+            }
+
+            arr[k] = D;
+            return arr;
+        }
+    }
+}
